a49c37ce07a467bf641e62f4fda7442db5ea7e2c
xuyuan.liang <xuyuan.liang@chinafsl.com>
2023-02-14 13:58:14 +0800

    创建新项目

---
+++ 01-MoreResourceDictionary/MoreResourceDictionary/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MoreResourceDictionary.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace MoreResourceDictionary
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
