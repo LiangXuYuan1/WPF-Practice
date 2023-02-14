f5c67c5978fd0dab2e89c8b1a3a9cfa3eb1d2c7f
xuyuan.liang <xuyuan.liang@chinafsl.com>
2023-02-14 17:23:42 +0800

    实现设计是数据显示

--- 05-DesignData/DesignData/ViewModels/MainWindowViewModel.cs
+++ 05-DesignData/DesignData/ViewModels/MainWindowViewModel.cs
@@ -11,9 +11,26 @@ namespace DesignData.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        private string data1;
+
+        public string Data1
+        {
+            get => data1;
+            set => SetProperty(ref data1, value);
+        }
+
+        private string data2;
+
+        public string Data2
+        {
+            get => data2;
+            set => SetProperty(ref data2, value);
+        }
+
         public MainWindowViewModel()
         {
-
+            Data1 = "String1 From MainWindowViewModel";
+            Data2 = "String2 From MainWindowViewModel";
         }
     }
 }
