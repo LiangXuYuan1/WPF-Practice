ad33f651b9dc13b2713713076c346cbcdce81c5e
xuyuan.liang <xuyuan.liang@chinafsl.com>
2023-02-15 09:38:35 +0800

    添加测试命令

--- 06-Binding/Binding/ViewModels/MainWindowViewModel.cs
+++ 06-Binding/Binding/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 
 namespace Binding.ViewModels
 {
@@ -11,9 +12,16 @@ namespace Binding.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
+        public DelegateCommand TestCommand { get; set; }
+
         public MainWindowViewModel()
         {
+            TestCommand = new DelegateCommand(Test);
+        }
 
+        private static void Test()
+        {
+            System.Diagnostics.Debug.WriteLine("Test");
         }
     }
 }
