d02ff5cae6759e882387b185cc28a34156672ce3
xuyuan.liang <xuyuan.liang@chinafsl.com>
2023-02-15 13:44:48 +0800

    定义一些测试类型

--- 08-DesignTimeViewModel/DesignTimeViewModel/ViewModels/MainWindowViewModel.cs
+++ 08-DesignTimeViewModel/DesignTimeViewModel/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Documents;
+using Prism.Mvvm;
 
 namespace DesignTimeViewModel.ViewModels
 {
@@ -10,6 +13,42 @@ namespace DesignTimeViewModel.ViewModels
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
+
+        // challenge 1: a string
+
+        private string testString;
+
+        public string TestString
+        {
+            get => testString;
+            set => SetProperty(ref testString, value);
+        }
+
+        // challenge 2: a string List
+
+        private List<string> testStringList;
+
+        public List<string> TestStringList
+        {
+            get => testStringList;
+            set => SetProperty(ref testStringList, value);
+        }
+
+        // challenge 3: a string ObservableCollection
+
+        private ObservableCollection<string> testStringObservableCollection;
+
+        public ObservableCollection<string> TestStringObservableCollection
+        {
+            get => testStringObservableCollection;
+            set => SetProperty(ref testStringObservableCollection, value);
+        }
+
+        // challenge 4: a custom class
+
+        // challenge 5: a custom class List
+
+        // challenge 6: a super complex custom class List
 
         public MainWindowViewModel()
         {
